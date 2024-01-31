3429ce794cbcc871033f125201378c236fd06b7a
Vitor Joaquim <vitoreojoaquim@gmail.com>
2024-01-31 16:09:04 -0300

    configurando Mamifero e Animal

---
+++ src/model/Animal.ts
@@ -0,0 +1,39 @@
+class Animal{
+
+
+    private nome: string;
+    private idade: number;
+    private genero: string;
+    
+    constructor(_nome: string,
+                _idade: number,
+                _genero: string) {
+        this.nome = _nome;
+        this.idade = _idade;
+        this.genero = _genero;
+    }
+
+    public getNome(): string {
+        return this.nome;
+    }
+    
+    public setNome(_nome: string): void {
+        this.nome = _nome;
+    }
+    
+    public getIdade(): number {
+        return this.idade;
+    }
+    
+    public setIdade(_idade: number): void {
+        this.idade = _idade;
+    }
+    
+    public getGenero(): string {
+        return this.genero;
+    }
+    
+    public setGenero(_genero: string): void {
+        this.genero = _genero;
+    }
+}
---
+++ src/model/Mamifero.ts
@@ -0,0 +1,19 @@
+class Mamifero extends Animal {
+    private raca: string;
+
+    constructor(_raca: string,
+                _nome: string,
+                _idade: number,
+                _genero: string) {
+        super(_nome, _idade, _genero);
+        this.raca = _raca;
+        }
+
+        public getRaca(): string {
+            return this.raca;
+        }
+        
+        public setRaca(_raca: string): void {
+            this.raca = _raca;
+        }
+}
