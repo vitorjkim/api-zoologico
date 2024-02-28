969cb93ddc091be163979eda23ea2ebc41446e02
Vitor Joaquim <vitoreojoaquim@gmail.com>
2024-02-28 14:27:08 -0300

    deletando a clsse zoologico

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { Habitat } from "./model/Habitat";
-import { Atracao } from "./model/Atracao";
 import { DatabaseModel } from "./model/DatabaseModel";
 import { Reptil } from "./model/Reptil";
 import { Mamifero } from "./model/Mamifero";
@@ -15,20 +13,6 @@ server.use(cors());
 
 server.get('/', (req, res) => {
     res.json("ola");
-});
-
-server.post('/habitat', (req, res) => {
-    const { nome, animais } = req.body;
-    const habitat = new Habitat(nome, animais);
-    console.log(habitat);
-    res.status(200).json('Habitat criado');
-});
-
-server.post('/atracao', (req, res) => {
-    const { nome, habitat } = req.body;
-    const atracao = new Atracao(nome, habitat);
-    console.log(atracao);
-    res.status(200).json('Atração criada');
 });
 
 server.get('/list/reptil', async (req, res) => {
--- src/model/Atracao.ts
+++ src/model/Atracao.ts
@@ -1,65 +1,153 @@
+import { DatabaseModel } from "./DatabaseModel";
 import { Habitat } from "./Habitat";
 
 /**
- * Classe Atracao representa uma atração dentro de um zoológico, composta por um nome e uma lista de habitats.
+ * Pool de conexão do banco de dados
+ */
+const database = new DatabaseModel().pool;
+
+/**
+ * Representa uma atração em um zoológico.
  */
 export class Atracao {
 
     /**
-     * Representa o nome da atração.
+     * O nome da atração.
      */
-    private nome: string;
+    private nomeAtracao: string;
 
     /**
-     * Lista de habitats associados à atração.
+     * A lista de atracaos presentes na atração.
      */
-    private lista_de_habitats: Array<Habitat>;
-    
+    private habitatAtracao: Habitat = new Habitat("");
+
     /**
-     * Construtor da classe Atracao.
+     * Cria uma nova instância de Atracao.
      * 
      * @param _nome O nome da atração.
-     * @param _lista_de_habitats A lista de habitats associados à atração.
+     * @param _atracaos A lista de atracaos presentes na atração.
      */
-    constructor(_nome: string,
-                _lista_de_habitats: Array<Habitat>) {
-        this.nome = _nome;
-        this.lista_de_habitats = _lista_de_habitats;
+    constructor(_nome: string) {
+        this.nomeAtracao = _nome;
     }
 
     /**
-     * Retorna o nome do Atracao.
+     * Obtém o nome da atração.
      * 
-     * @returns o nome do Atracao.
+     * @returns O nome da atração.
      */
-    public getNome(): string {
-        return this.nome;
+    public getNomeAtracao(): string {
+        return this.nomeAtracao;
     }
-    
+
     /**
-     * Define o nome do Atracao.
+     * Define o nome da atração.
      * 
-     * @param nome O nome a ser atribuído ao Atracao.
+     * @param _nomeAtracao O nome a ser atribuído à atração.
      */
-    public setNome(_nome: string): void {
-        this.nome = _nome;
+    public setNomeAtracao(_nomeAtracao: string): void {
+        this.nomeAtracao = _nomeAtracao;
     }
-    
-     /**
-     * Retorna a lista de habitats do Atracao.
-     * 
-     * @returns A lista de habitats do Atracao.
-     */
-    public getLista_de_habitats(): Array<Habitat> {
-        return this.lista_de_habitats;
-    }
-    
+
     /**
-     * Define a lista de habitats no Atracao.
+     * Obtém a lista de atracaos presentes na atração.
      * 
-     * @param lista_de_habitats A lista de habitats a ser atribuída ao Atracao.
+     * @returns A lista de atracaos da atração.
      */
-    public setLista_de_habitats(_lista_de_habitats: Array<Habitat>): void {
-        this.lista_de_habitats = _lista_de_habitats;
+    public getatracaos(): Habitat {
+        return this.habitatAtracao;
+    }
+
+    /**
+     * Define a lista de atracaos da atração.
+     * 
+     * @param _atracaos A lista de atracaos a ser atribuída à atração.
+     */
+    public setatracao(_habitat: Habitat): void {
+        this.habitatAtracao = _habitat;
+    }
+
+    /**
+     * Retorna uma lista com todos as atrações cadastradas no banco de dados
+     * 
+     * @returns Lista com todos as atrações cadastradas no banco de dados
+     */
+    static async listarAtracoes(): Promise<any> {
+        // Cria uma lista (array) vazia do tipo atracao
+        const listaDeAtracoes: Array<Atracao> = [];
+
+        // Construção da query para selecionar as informações de um atracao
+        const querySelectAtracao = `SELECT Atracao.idAtracao, Atracao.nomeAtracao, Habitat.nomeHabitat 
+                                    FROM Atracao
+                                    LEFT JOIN Habitat ON Atracao.idHabitat = Habitat.idHabitat`;
+
+        try {
+            // Faz a consulta no banco de dados e retorna o resultado para a variável queryReturn
+            const queryReturn = await database.query(querySelectAtracao);
+            // Percorre todas as linhas da queryReturn e acessa cada objeto individualmente
+            queryReturn.rows.forEach(atracao => {
+                // Coloca o objeto dentro da lista de atrações
+                listaDeAtracoes.push(atracao);
+            });
+
+            // retorna a lista de atrações para quem chamou a função
+            return listaDeAtracoes;
+        } catch (error) {
+            // Caso dê algum erro na query do banco, é lançado o erro para quem chamou a função
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error, verifique os logs do servidor";
+        }
+    }
+
+    /**
+     * Assinatura de métodos
+     */
+    static async cadastrarAtracao(atracao: Atracao): Promise<any>;
+    static async cadastrarAtracao(atracao: Atracao, idHabitat: Habitat): Promise<any>;
+
+    /**
+     * Cadastra um objeto do tipo Atracao no banco de dados
+     * 
+     * @param atracao Objeto do tipo Atracao
+     * @returns **true** caso sucesso, **false** caso erro
+     */
+    static async cadastrarAtracao(atracao: Atracao, idHabitat?: Habitat): Promise<any> {
+        // Cria uma variável do tipo booleano para guardar o status do resultado da query
+        let insertResult = false;
+        let queryInsertAtracao: string;
+        try {
+            if(!idHabitat) {
+                // Construção da query para inserir as informações de um Mamifero. A query irá retornar o ID gerado para o animal pelo banco de dados
+                queryInsertAtracao = `INSERT INTO atracao (nomeatracao) 
+                                            VALUES 
+                                            ('${atracao.getNomeAtracao().toUpperCase()}');`;
+            } else {
+                // Construção da query para inserir as informações de um Mamifero. A query irá retornar o ID gerado para o animal pelo banco de dados
+                queryInsertAtracao = `INSERT INTO atracao (nomeatracao, idhabitat) 
+                                            VALUES 
+                                            ('${atracao.getNomeAtracao().toUpperCase()}', ${idHabitat});`;
+            }
+
+            // Faz a query de insert no banco de dados, passando para o banco as informações do objeto recebibo como parâmetro pela função
+            await database.query(queryInsertAtracao)
+                // Testa para ter certeza que foi possível inserir os dados no banco
+                .then((resultAtracao) => {
+                    if (resultAtracao.rowCount != 0) {
+                        // Se o número de linhas for diferente de zero, a operação deu certo e o valor VERDADEIRO é atribuido na variável
+                        insertResult = true;
+                    }
+                });
+
+            console.log(insertResult);
+            // Retorna VERDADEIRO para quem chamou a função, indicando que a operação foi realizada com sucesso
+            return insertResult;
+        } catch (error) {
+            // Imprime o erro no console
+            console.log(error, insertResult);
+
+            // Caso a inserção no banco der algum erro, é restorno o valor FALSO para quem chamou a função
+            return insertResult;
+        }
     }
 }
