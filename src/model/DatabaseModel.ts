3f421ba65c734e9bd7febc68b10d1fbdeed0c390
Vitor Joaquim <vitoreojoaquim@gmail.com>
2024-03-27 13:46:22 -0300

    adicionado o .env na aplicação para a segurança dos dados

--- src/model/DatabaseModel.ts
+++ src/model/DatabaseModel.ts
@@ -11,11 +11,11 @@ export class DatabaseModel {
 
     constructor() {
         this._config = {
-            user: 'postgres',
-            host: 'localhost',
-            database: 'zoologico',
-            password: 'admin',
-            port: 5432,
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
             max: 10,
             idleTimoutMillis: 10000
         }
